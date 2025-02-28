172edae589400e3bc899175a4e7ce3774bb87e26
dwesh163 <ehouarn.duriaux@protonmail.com>
2025-02-28 14:40:08 +0100

    [update] enhance Node and Pod components to handle node status and improve layout

--- ui/src/components/Node.tsx
+++ ui/src/components/Node.tsx
@@ -1,27 +1,26 @@
 import { cn } from '@/lib/utils';
 import { NodeType } from '@/types/k3s';
-import { Badge } from './ui/badge';
 import { Card, CardContent } from './ui/card';
 import { Pod } from './Pod';
 
-export const Node: React.FC<{ node: NodeType }> = ({ node }) => {
+export const Node: React.FC<{ node: NodeType; size: number }> = ({ node, size }) => {
 	const getNodeColor = (status: string) => {
-		if (status === 'Down') return 'bg-red-500';
+		if (status === 'Down' || status === 'Unknown') return 'bg-red-500';
 		return 'bg-green-500';
 	};
 
 	return (
-		<Card className={cn(getNodeColor(node.status), 'border-0 shadow-xl overflow-hidden h-full')}>
+		<Card className={cn(getNodeColor(node.status), 'border-0 shadow-xl overflow-y-auto h-[calc(50vh-4rem)]')}>
 			<CardContent className="p-6">
 				<div className="text-white">
 					<div className="flex items-center justify-between mb-4">
-						<h2 className="text-lg font-bold">{node.name}</h2>
-						<Badge className="bg-white/20">{node.status}</Badge>
+						<h2 className="text-lg font-bold truncate w-full" title={node.name}>
+							{node.name}
+						</h2>
 					</div>
-
-					<div className="mt-4 grid grid-cols-2 gap-3">
+					<div className="grid gap-3" style={{ gridTemplateColumns: `repeat(${size}, minmax(0, 1fr))` }}>
 						{node.pods.map((pod, idx) => (
-							<Pod key={'Pod' + idx} pod={pod} />
+							<Pod key={'Pod' + idx} pod={pod} node={node} />
 						))}
 					</div>
 				</div>
--- ui/src/components/Pod.tsx
+++ ui/src/components/Pod.tsx
@@ -1,21 +1,30 @@
 import { cn } from '@/lib/utils';
-import { PodType } from '@/types/k3s';
+import { NodeType, PodType } from '@/types/k3s';
 
-export const Pod: React.FC<{ pod: PodType }> = ({ pod }) => {
-	const getPodStatusColor = (status: string) => {
+export const Pod: React.FC<{ pod: PodType; node: NodeType }> = ({ pod, node }) => {
+	const getPodStatusColor = (status: string, nodeStatus: string) => {
+		if (nodeStatus === 'false' || nodeStatus === 'Unknown') {
+			return 'bg-white/10';
+		}
+
 		switch (status) {
 			case 'Running':
 				return 'bg-emerald-400';
 			case 'Starting':
 				return 'bg-yellow-400';
+			case 'CrashLoopBackOff':
+				return 'bg-red-600';
 			default:
 				return 'bg-white/10';
 		}
 	};
 
 	return (
-		<div className={cn('bg-white/10 rounded-lg aspect-square flex flex-col items-center justify-center', getPodStatusColor(pod.status))}>
-			<span className="text-sm uppercase font-bold">{pod.name}</span>
+		<div className={cn('bg-white/10 rounded-lg aspect-square flex flex-col items-center justify-center p-3', getPodStatusColor(pod.status, node.status))}>
+			<span className="text-xs uppercase font-bold truncate w-full text-center" title={pod.name}>
+				{pod.name}
+			</span>
+			<span className={'text-xs text-gray-100'}>{node.status === 'false' || node.status === 'Unknown' ? 'Down' : pod.status}</span>
 		</div>
 	);
 };
