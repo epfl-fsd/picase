4022ab3abe0e89ea993970e700443fa7300c4fb1
dwesh163 <ehouarn.duriaux@protonmail.com>
2025-02-28 12:50:24 +0100

    [add] extend type definitions for Pod, Event, and Deployment in k3s

--- ui/src/types/k3s.ts
+++ ui/src/types/k3s.ts
@@ -9,4 +9,23 @@ export interface NodeType {
 export interface PodType {
 	name: string;
 	status: string;
+	age: Date;
+	node: string;
+}
+
+export interface EventType {
+	eventType: string;
+	resource: string;
+	name: string;
+	status: string;
+	message: string;
+	date: Date;
+}
+
+export interface DeploymentType {
+	name: string;
+	replicas: number;
+	availableReplicas: number;
+	unavailableReplicas: number;
+	age: Date;
 }
